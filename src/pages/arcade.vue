4a68781538d696783ed40ab8e0327bbb7dfaffb8
fengzhuojia <179309463@qq.com>
2020-05-17 16:56:10 +0800

    first commit

---
+++ src/pages/arcade.vue
@@ -0,0 +1,15 @@
+<template>
+    <AppstorePage title="Arcade">
+      <f7-block>Work in progress</f7-block>
+    </AppstorePage>
+</template>
+
+<script>
+import AppstorePage from '../components/appstore-page.vue';
+
+export default {
+    components: {
+        AppstorePage,
+    }
+}
+</script>
---
+++ src/pages/search.vue
@@ -0,0 +1,15 @@
+<template>
+    <AppstorePage title="Search">
+      <f7-block>Work in progress</f7-block>
+    </AppstorePage>
+</template>
+
+<script>
+import AppstorePage from '../components/appstore-page.vue';
+
+export default {
+    components: {
+        AppstorePage,
+    }
+}
+</script>
